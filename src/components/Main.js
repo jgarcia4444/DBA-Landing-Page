e87087277d241ed1c657d19c93ad1696e406a6b1
github username <bballwiz4@gmail.com>
2020-09-08 22:35:04 -0700

    Different images now added for the app screeshots.

---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import dailyQuote from './imgs/dailyQuote.PNG';
+import searchQuote from './imgs/searchQuote.PNG';
+import randomQuote from './imgs/randomQuote.PNG';
+
+const Main = (props) => {
+    const screenshots = [dailyQuote, searchQuote, randomQuote]
+    return (
+        <div className="main">
+            <div className="row">
+                <div className="half-col">
+                    <h4>An Inspirational Quote Genrator</h4>
+                </div>
+                <div className="half-col">
+                    <p className="intro">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit provident blanditiis obcaecati veritatis, ducimus sit tenetur culpa maxime incidunt pariatur nostrum, sed voluptas enim natus possimus consectetur rem, labore totam!
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit provident blanditiis obcaecati veritatis, ducimus sit tenetur culpa maxime incidunt pariatur nostrum, sed voluptas enim natus possimus consectetur rem, labore totam!
+                    </p>
+                </div>
+            </div>
+            <div className="row">
+                <h3 id="screenshot-title">The App</h3>
+                {screenshots.map(img => <img className="screenshot" src={img} alt="app screen shot" /> )}
+            </div>
+        </div>
+    );
+}
+
+export default Main;
---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Navbar extends Component {
+
+
+    render() {
+        return (
+            <div className="title-container">
+                <h1 className="title">Dream Believe Achieve</h1>
+            </div>
+        );
+    }
+}
