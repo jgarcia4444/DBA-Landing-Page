37dd1a23e845bca7221b781566f7ad06b491556f
github username <bballwiz4@gmail.com>
2020-09-10 17:52:35 -0700

    Intro written and laid oout properly dividers added to show different sections on the page

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,22 +11,26 @@ const Main = () => {
         <div className="main">
             <div className="row">
                 <div className="half-col">
+                <div className="intro-container">
+                    <h4 id="intro-header">
+                        An Inspirational Quote Genrator
+                    </h4>
                     <p className="intro">
-                        <h4>
-                            An Inspirational Quote Genrator
-                        </h4>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit provident blanditiis obcaecati veritatis, ducimus sit tenetur culpa maxime incidunt pariatur nostrum, sed voluptas enim natus possimus consectetur rem, labore totam!
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit provident blanditiis obcaecati veritatis, ducimus sit tenetur culpa maxime incidunt pariatur nostrum, sed voluptas enim natus possimus consectetur rem, labore totam!
+                        This app is meant to inspire others and motivate them to become what they want to be. Sometimes you need to read some wisdom from generations past to really see the whole picture. Hopefully these quotes can help inspire someone to achieve their dreams. Through the social media sharing options on this app users can help spread the motivation to aspire to be more to their friends and extended network.
                     </p>
+                </div>
+                    
                 </div>
             </div>
             <div className="row">
                 <img id="download" src={download} alt="download on the app store"/>
             </div>
+            <hr />
             <div className="row">
                 <h3 id="screenshot-title">The App</h3>
                 {screenshots.map(img => <img className="screenshot" src={img} alt="app screen shot" /> )}
             </div>
+            <hr />
             <div className="row">
                 <div className="bio-card">
                     <h3 id="chloe-title">The Designer</h3>
